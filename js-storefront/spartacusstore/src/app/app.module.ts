30e14bd8b663104d42141cfe29ae9b262ec56718
Bill <Xymmer@users.noreply.github.com>
2020-09-18 17:11:44 -0400

    Add files via upload

--- js-storefront/spartacusstore/src/app/app.module.ts
+++ js-storefront/spartacusstore/src/app/app.module.ts
@@ -16,21 +16,23 @@ import { B2cStorefrontModule } from '@spartacus/storefront';
         occ: {
           // The following line (baseUrl) is commented out because the build process will add it automatically (set in index.html at meta=occ-backend-base-url)
           // baseUrl: 'https://localhost:9002',
+
+          // the prefix is /rest/v2/ with 1905, and by default /occ/v2 with 2005
           prefix: '/occ/v2/'
         }
       },
-       context: {
-         urlParameters: ['baseSite', 'language', 'currency'],
-         baseSite: ['electronics-spa','apparel-uk-spa'],
-         currency: ['USD', 'GBP',]
-       },
+      context: {
+        urlParameters: ['baseSite', 'language', 'currency'],
+        baseSite: ['electronics-spa','apparel-uk-spa'],
+        currency: ['USD', 'GBP',]
+      },
       i18n: {
         resources: translations,
         chunks: translationChunksConfig,
         fallbackLang: 'en'
       },
       features: {
-        level: '2.0'
+        level: '3.0'
       }
     })
   ],
