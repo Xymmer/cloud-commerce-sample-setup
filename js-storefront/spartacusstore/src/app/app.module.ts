9d3e235e1e06b19b841f992c7c1b275fcf608874
Bill <Xymmer@users.noreply.github.com>
2020-09-19 10:05:04 -0400

    Add files via upload

--- js-storefront/spartacusstore/src/app/app.module.ts
+++ js-storefront/spartacusstore/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -10,21 +10,17 @@ import { B2cStorefrontModule } from '@spartacus/storefront';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     B2cStorefrontModule.withConfig({
       backend: {
         occ: {
-          // The following line (baseUrl) is commented out because the build process will add it automatically (set in index.html at meta=occ-backend-base-url)
-          // baseUrl: 'https://localhost:9002',
-
-          // the prefix is /rest/v2/ with 1905, and by default /occ/v2 with 2005
-          prefix: '/occ/v2/'
+          baseUrl: 'https://localhost:9002',
+          prefix: '/rest/v2/'
         }
       },
       context: {
-        urlParameters: ['baseSite', 'language', 'currency'],
-        baseSite: ['electronics-spa','apparel-uk-spa'],
-        currency: ['USD', 'GBP',]
+        currency: ['USD'],
+        language: ['en'],
       },
       i18n: {
         resources: translations,
@@ -34,7 +30,8 @@ import { B2cStorefrontModule } from '@spartacus/storefront';
       features: {
         level: '3.0'
       }
-    })
+    }),
+    BrowserTransferStateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
