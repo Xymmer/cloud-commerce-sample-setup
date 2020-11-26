06fdc112cc1b3825501367b0e788e358d483ae08
Bill <bill.marcotte@sap.com>
2020-11-26 15:52:02 -0500

    add b2bstore

---
+++ js-storefront/b2bstore/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'b2bstore'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('b2bstore');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('b2bstore app is running!');
+  });
+});
---
+++ js-storefront/b2bstore/src/app/app.module.ts
@@ -0,0 +1,76 @@
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { translations, translationChunksConfig } from '@spartacus/assets';
+import { B2bStorefrontModule, defaultB2bOccConfig } from '@spartacus/setup';
+import { provideDefaultConfig } from '@spartacus/core';
+import { AdministrationRootModule } from '@spartacus/organization/administration/root';
+import { provideConfig } from '@spartacus/core';
+import { organizationTranslations } from '@spartacus/organization/administration/assets';
+import { organizationTranslationChunksConfig } from '@spartacus/organization/administration/assets';
+import { OrderApprovalRootModule } from '@spartacus/organization/order-approval/root';
+import { orderApprovalTranslations } from '@spartacus/organization/order-approval/assets';
+import { orderApprovalTranslationChunksConfig } from '@spartacus/organization/order-approval/assets';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    B2bStorefrontModule.withConfig({
+      featureModules: {
+        organizationOrderApproval: {
+        module: () => import('@spartacus/organization/order-approval').then(
+          (m) => m.OrderApprovalModule
+        ),
+        },
+        organizationAdministration: {
+          module: () => import('@spartacus/organization/administration').then(
+          (m) => m.AdministrationModule
+        ),
+        },
+      },
+      backend: {
+        occ: {
+ //         baseUrl: 'https://localhost:9002'
+        }
+      },
+      context: {
+        urlParameters: ['baseSite', 'language', 'currency'],
+        baseSite: ['powertools-spa'],
+        language: ['en'],
+        currency: ['USD']
+      },
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en'
+      },
+      features: {
+        level: '3.0'
+      }
+    }),
+    BrowserTransferStateModule,
+    AdministrationRootModule,
+    OrderApprovalRootModule,
+  ],
+  providers: [provideDefaultConfig(defaultB2bOccConfig),
+    provideConfig({
+      i18n: {
+        resources: organizationTranslations,
+        chunks: organizationTranslationChunksConfig,
+      },
+    }),
+    
+    provideConfig({
+      i18n: {
+        resources: orderApprovalTranslations,
+        chunks: orderApprovalTranslationChunksConfig,
+      },
+    })],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
